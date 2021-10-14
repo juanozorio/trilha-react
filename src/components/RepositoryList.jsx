856030cce2a8fdc4072d2a0834354a03afab9462
juanozorio <juan_ozorio@outlook.com>
2021-10-13 21:37:59 -0300

    Complemento com chamada HTTP

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,4 +1,7 @@
+import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
+
+import '../styles/repositories.scss';
 
 const repository = {
     name: 'unform',
@@ -7,15 +10,27 @@ const repository = {
 }
 
 export function RepositoryList() {
+    const [repositories, setRepositories] = useState([])
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/juanozorio/repos')
+        .then(response => response.json())
+        .then(data => setRepositories(data))
+    }, [repositories])
+    // [] Nunca esquecer dessa dependência
+
+
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
                 <ul>
-                    <RepositoryItem repository={repository}/>
-                    <RepositoryItem repository={repository}/>
-                    <RepositoryItem repository={repository}/>
-                    <RepositoryItem repository={repository}/>
+                    {repositories.map(repository => {
+                        return<RepositoryItem key={repository.name} repository={repository}/>
+                    })}
+
             </ul>
         </section>
     );
 }
+
+
